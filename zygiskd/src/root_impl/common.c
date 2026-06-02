8034041f8b0f66dd386fa90fe8d76ec504ffcdd9
Jordan231111 <yejordan8888@gmail.com>
2026-06-02 08:31:26 -0400

    Revert "remove: Magisk Kitsune support"
    This reverts commit 333d423cde1a959958d9fce380bd017cf6c1cf64.

--- zygiskd/src/root_impl/common.c
+++ zygiskd/src/root_impl/common.c
@@ -57,7 +57,11 @@ void root_impls_setup(void) {
       break;
     }
     case Magisk: {
-      LOGI("Magisk root implementation found.\n");
+      if (state_magisk.variant == 0) {
+        LOGI("Magisk Official root implementation found.\n");
+      } else {
+        LOGI("Magisk Kitsune root implementation found.\n");
+      }
 
       break;
     }
